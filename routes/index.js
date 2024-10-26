e61d502b13de145a61c0e3d80f93dff2f910ddfc
ThankGod George <thankgod@flutterwavego.com>
2024-10-26 01:01:54 +0100

    made new changes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const me = require('../package.json')
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   res.json({ 
-    status : 'successful',
-    message: 'This is We-Pray API',
-    data: null
+    status: 'successful', 
+    message: 'Retrieved message successfully', 
+    data: {
+      name: me.name, 
+      version: me.version
+    } 
   });
 });
 
